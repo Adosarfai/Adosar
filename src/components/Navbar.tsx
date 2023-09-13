6f869e7e703217dd773f9d65f3cf04f8c965a001
thijnmens <thijnsmulders04@gmail.com>
2023-09-13 15:07:36 +0200

    basic layout

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,36 @@
+﻿export default function Navbar() {
+	const pages = [
+		{ display: 'Home', page: '/' },
+		{ display: 'Leaderboards', page: '/leaderboards' },
+		{ display: 'Users', page: '/users' },
+		{ display: 'Packs', page: '/packs' },
+	];
+
+	return (
+		<nav className='w-screen h-24 bg-gray-800 shadow-2xl'>
+			<div className='flex gap-12'>
+				<img
+					src='logo128.png'
+					alt='logo'
+					width='96'
+					height='96'
+					className='mx-5'
+				/>
+				{pages.map((page, index) => {
+					return (
+						<a
+							href={page.page}
+							key={index}
+							className={`px-4 py-2 my-auto bg-white bg-opacity-0 hover:bg-opacity-10 hover:text-orange-400 rounded-lg animate ${
+								page.page == window.location.pathname
+									? 'font-bold'
+									: 'font-extralight'
+							}`}>
+							{page.display}
+						</a>
+					);
+				})}
+			</div>
+		</nav>
+	);
+}
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		extend: {},
+	},
+	colors: {
+		charcoal: '#36454F',
+		primary: '#FB8B24',
+		secondary: '#DB5461',
+		linen: '#FAF0E6',
+		cambridge: '#7C9885',
+	},
+	plugins: [],
+};
