45827fb205f49c6eae68f1beb9d73e3daf4b58c8
thijnmens <thijnsmulders04@gmail.com>
2023-11-26 21:54:25 +0100

    Map page and user search

---
+++ src/components/Usercard.tsx
@@ -0,0 +1,48 @@
+﻿import { FiExternalLink } from 'react-icons/fi';
+
+interface Props {
+	userId: number;
+	username: string;
+	creationDate: string;
+	description: string;
+	innerClassName?: string;
+}
+
+export default function Usercard(props: Props) {
+	return (
+		<div
+			className={
+				props.innerClassName
+					? props.innerClassName
+					: 'rounded-2xl bg-gray-800 p-6 my-6 flex-1 sm:flex sm:max-w-[33%]'
+			}>
+			<img
+				src={`${import.meta.env.VITE_CDN_URL}/user/${props.userId}.png`}
+				alt='pfp'
+				className='h-fit w-32 rounded-full mr-8 shadow-2xl'
+			/>
+			<div className='relative w-full'>
+				<div className='text-sm sm:text-2xl flex justify-between'>
+					<h1 className='font-bold mx-auto mt-2 sm:mx-0 sm:mt-0 pb-1'>
+						{props.username}
+					</h1>
+					<p className='font-light text-sm hidden sm:block'>
+						{new Date(props.creationDate).toDateString()}
+					</p>
+				</div>
+				<hr />
+				<p className='font-light text-gray-300 text-xl hidden sm:block'>
+					{props.description}
+				</p>
+				<div className='sm:absolute bottom-0 right-0 flex gap-4 justify-evenly mt-2'>
+					<a
+						className='hover:opacity-30 smooth duration-100'
+						href={`/user/${props.userId}`}
+						target='_blank'>
+						<FiExternalLink />
+					</a>
+				</div>
+			</div>
+		</div>
+	);
+}
--- src/routes/users/index.tsx
+++ src/routes/users/index.tsx
@@ -1,7 +1,64 @@
-﻿export default function Users() {
+﻿import UserService from '@services/UserService.ts';
+import { useState } from 'react';
+import Loading from '@components/Loading.tsx';
+import { User } from '@classes/user.ts';
+import Usercard from '@components/Usercard.tsx';
+import { toast } from 'react-toastify';
+import { useDebounce } from 'use-debounce';
+
+export default function Users() {
+	const [page, setPage] = useState<number>(0);
+	const [usernameQuery, setUsernameQuery] = useState<string>('');
+	const [usernameQueryBounce] = useDebounce<string>(usernameQuery, 250);
+
+	const { data: users, error } = UserService.getAllUsersWithPartialData(
+		page,
+		usernameQueryBounce
+	);
+
+	if (error) {
+		toast.error(`Could not load users`);
+		return (
+			<div className='m-16 bg-gray-800 rounded-2xl p-8 flex'>
+				<h1 className='mx-auto'>‼️Users could not be loaded‼️</h1>
+			</div>
+		);
+	}
 	return (
-		<div>
-			<h1>Users</h1>
+		<div className='m-16 flex gap-4'>
+			<div className='bg-gray-800 rounded-lg p-4 min-w-[70%]'>
+				Users
+				<hr />
+				<div className='m-4'>
+					{!users ? (
+						<Loading />
+					) : (
+						users.map((user: User, i: number) => {
+							return (
+								<Usercard
+									key={i}
+									userId={user.userId}
+									username={user.username}
+									creationDate={user.creationDate}
+									description={user.description}
+									innerClassName='rounded-2xl bg-gray-700 p-6 my-6 flex-1 sm:flex sm:max-w-full'
+								/>
+							);
+						})
+					)}
+				</div>
+			</div>
+			<div className='bg-gray-800 rounded-lg p-4 w-full'>
+				Filters
+				<hr />
+				<input
+					className='my-2 rounded-lg px-2 py-1 bg-gray-700'
+					type='text'
+					onChange={e => setUsernameQuery(e.currentTarget.value)}
+					placeholder='Search'
+				/>
+			</div>
+			<button onClick={() => setPage(1)}>&gt;</button>
 		</div>
 	);
 }
