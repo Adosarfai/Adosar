11ca84fdaa3bfa31860066aa943a9ac0e6b0bcda
Thijnmens <thijnsmulders04@gmail.com>
2024-01-17 08:35:35 +0100

    Websocket feed page (will remove later, needed for school)

---
+++ src/routes/Feed.tsx
@@ -0,0 +1,63 @@
+import { Signal, signal } from '@preact/signals-react';
+import { IFrame, Stomp } from '@stomp/stompjs';
+import { Score } from '@classes/score.ts';
+import { useEffect } from 'react';
+import ScoreCard from '@components/ScoreCard.tsx';
+
+const connected: Signal<boolean> = signal(false);
+const messages: Signal<Score[]> = signal([]);
+
+export default function Feed() {
+	function onMessage(message: Score) {
+		messages.value = [...messages.value, message];
+	}
+
+	function connect() {
+		let StompClient = Stomp.over(new WebSocket('ws://localhost:8080/ws'));
+
+		StompClient.onConnect = (_: IFrame) => {
+			connected.value = true;
+			console.log(`Connected to ws`);
+			// @ts-ignore Cannot be null
+			StompClient.subscribe('/feed', message => {
+				onMessage(JSON.parse(message.body));
+			});
+		};
+
+		StompClient.onWebSocketError = error => {
+			console.error('Error with websocket', error);
+		};
+
+		StompClient.onStompError = frame => {
+			console.error('Broker reported error: ' + frame.headers['message']);
+			console.error('Additional details: ' + frame.body);
+		};
+
+		StompClient.activate();
+	}
+
+	useEffect(() => {
+		connect();
+	}, []);
+
+	return (
+		<div className='w-full'>
+			<h1 className='mx-auto w-fit'>New Scores</h1>
+			<div className='flex justify-evenly'>
+				{messages.value.map((score, i) => {
+					return (
+						<div key={i}>
+							<ScoreCard
+								userId={score.user.userId}
+								replayId={score.replay.replayId}
+								points={score.points}
+								speed={score.speed}
+								username={score.user.username}
+							/>
+						</div>
+					);
+				})}
+			</div>
+		</div>
+	);
+}
